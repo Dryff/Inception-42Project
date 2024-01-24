563b6ca6d5a4c420e64ae723e55da3e8296e689d
cgelin <cgelin@student.42lyon.fr>
2024-01-24 16:45:47 +0100

    server working

--- Makefile
+++ Makefile
@@ -4,21 +4,21 @@ all:	$(WP_VOLUME_PATH) $(MARIADB_VOLUME_PATH)
 		docker compose -f ./srcs/docker-compose.yml up --build
 
 down:
-	docker compose -f ./srcs/docker-compose.yml down -v
+		docker compose -f ./srcs/docker-compose.yml down -v
 
-re: fclean
-	$(MAKE) all
+re:		fclean
+		$(MAKE) all
 
 clean:
-	docker compose -f srcs/docker-compose.yml down --volumes --rmi all
+		docker compose -f srcs/docker-compose.yml down --volumes --rmi all
 
-fclean:
-	rm -rf $(WP_VOLUME_PATH) $(MARIADB_VOLUME_PATH)
+fclean:	clean
+		rm -rf $(WP_VOLUME_PATH) $(MARIADB_VOLUME_PATH)
 
 $(WP_VOLUME_PATH):
-	mkdir -p $(WP_VOLUME_PATH)
+		mkdir -p $(WP_VOLUME_PATH)
 
 $(MARIADB_VOLUME_PATH):
-	mkdir -p $(MARIADB_VOLUME_PATH)
+		mkdir -p $(MARIADB_VOLUME_PATH)
 
 .PHONY: all re down clean fclean
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,23 +1,25 @@
+#!/bin/bash
+
 # Execution du script si wp-config n'existe pas
-if [ ! -f "/var/www/wp-config.php" ]; then
+if [ ! -f "${WP_PATH}/wp-config.php" ]; then
 
   # Installation de Wordpress
   wp core download --path=$WP_PATH --allow-root
 
   # Configuration de Wordpress avec des variables d'environnement
-  wp config create --dbname=$SQL_DATABASE \
+  wp config create --allow-root \
+			  --dbname=$SQL_DATABASE \
               --dbuser=$SQL_USER \
               --dbpass=$SQL_PASSWORD \
               --dbhost=mariadb:3306 \
-              --allow-root \
               --path=$WP_PATH
 
-  wp core install --url="${WP_URL}" \
+  wp core install --allow-root \
+  			  --url="${WP_URL}" \
               --title="${WP_TITLE}" \
               --admin_user="${WP_ADMIN_USER}" \
               --admin_password="${WP_ADMIN_PASSWORD}" \
               --admin_email="${WP_ADMIN_EMAIL}" \
-              --allow-root \
               --path=$WP_PATH
 
   wp user create $WP_USER $WP_USER_EMAIL \
